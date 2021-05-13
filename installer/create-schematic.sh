4b87af95ec776991134f90be6eb22509719b9ce3
Johnnie-Hernandez <johnnie.hernandez@ibm.com>
2021-05-13 08:55:50 -0400

    added iaf

--- installer/create-schematic.sh
+++ installer/create-schematic.sh
@@ -53,8 +53,10 @@ CLOUD_PAK_REPO_LOCATION_AUTOMATION="https://github.com/ibm-hcbt/cloud-pak-sandbo
 
 EXISTING_CLUSTER="false"
 
-#IAF
-
+IAF="false"
+IAF_VERSION="IAF"
+IAF_TEMPLATE=./templates/iaf-workspace-configuration.json
+IAF_REPO_LOCATION="https://github.com/ibm-hcbt/cloud-pak-sandboxes/tree/master/terraform/iaf"
 IBM_API_KEY="none"
 
 
@@ -74,7 +76,7 @@ get_cloud_pak_install() {
     echo "${bold}This script will generate a ROKS cluster and install a specified cloud pak${normal}"
     echo ""
     echo "${bold}Select the cloud pack option to install${green}"
-    cloudPaks=("$CLOUD_PAK_NAME_MCM_VERSION" "$CLOUD_PAK_NAME_APP_VERSION" "$CLOUD_PAK_NAME_DATA_VERSION" "$CLOUD_PAK_NAME_DATA2_VERSION" "$CLOUD_PAK_NAME_INTEGRATION_VERSION" "$CLOUD_PAK_NAME_AUTOMATION_VERSION")
+    cloudPaks=("$CLOUD_PAK_NAME_MCM_VERSION" "$CLOUD_PAK_NAME_APP_VERSION" "$CLOUD_PAK_NAME_DATA_VERSION" "$CLOUD_PAK_NAME_DATA2_VERSION" "$CLOUD_PAK_NAME_INTEGRATION_VERSION" "$CLOUD_PAK_NAME_AUTOMATION_VERSION" "$IAF_VERSION")
     select cloudpak in "${cloudPaks[@]}"; do
         case $cloudpak in
             $CLOUD_PAK_NAME_MCM_VERSION)
@@ -137,6 +139,16 @@ get_cloud_pak_install() {
                 jq -r ".template_repo.branch |= \"master\"" temp.json > workspace-configuration.json
                 break
                 ;; 
+            $IAF_VERSION)
+                echo "${bold}Selected: $IAF_VERSION"
+                IAF="true"
+                cp $IAF_TEMPLATE workspace-configuration.json
+                cp workspace-configuration.json temp.json
+                jq -r --arg v "$IAF_REPO_LOCATION" '.template_repo.url |= $v' temp.json  > workspace-configuration.json
+                cp workspace-configuration.json temp.json
+                jq -r ".template_repo.branch |= \"master\"" temp.json > workspace-configuration.json
+                break
+                ;;     
             *) echo "${bold}invalid option $REPLY ${green}";;
         esac
     done
@@ -252,7 +264,13 @@ get_workspace_name() {
     then
         read -p "${bold}Enter Sandbox Name (sandbox name will be appended with ${green}-cp4auto-sandbox${bold}):${normal} " -e WORKSPACE_NAME
         WORKSPACE_NAME=$WORKSPACE_NAME"-cp4auto-sandbox"
-    fi    
+    fi
+
+    if $IAF
+    then
+        read -p "${bold}Enter Sandbox Name (sandbox name will be appended with ${green}-iaf-sandbox${bold}):${normal} " -e WORKSPACE_NAME
+        WORKSPACE_NAME=$WORKSPACE_NAME"-iaf-sandbox"
+    fi        
 }
 
 get_vpc() {
@@ -280,10 +298,10 @@ get_vpc() {
 
 get_ibm_api_key() {
     echo "${bold}Enter IBM Cloud API Key, for more instructions go to"
-    read -p "${green}https://github.com/ibm-hcbt/cloud-pak-sandboxes/tree/master/terraform#create-an-ibm-cloud-api-key:${normal} " -e IBM_API_KEY
+    read -s -p "${green}https://github.com/ibm-hcbt/cloud-pak-sandboxes/tree/master/terraform#create-an-ibm-cloud-api-key:${normal} " -e IBM_API_KEY
 
     cp workspace-configuration.json temp.json
-    jq -r --arg v "$ENTITLED_KEY" '(.template_data[] | .variablestore[] | select(.name == "entitled_registry_key") | .value) |= $v' temp.json > workspace-configuration.json    
+    jq -r --arg v "$IBM_API_KEY" '(.template_data[] | .variablestore[] | select(.name == "IC_API_KEY") | .value) |= $v' temp.json > workspace-configuration.json    
 
 }
 
@@ -299,6 +317,10 @@ get_meta_data() {
     read -s -p "${bold}Enter Entitled Registry key (retrieve from ${green}https://myibm.ibm.com/products-services/containerlibrary):${normal} " -e ENTITLED_KEY
     echo " "
     read -p "${bold}Enter Entitled Registry Email:${normal} " -e ENTITLED_EMAIL
+    if $IAF
+    then
+        get_ibm_api_key
+    fi 
 }
 
 # writes metadata to workspace-configuration.json and temp.json these need to be cleaned up later
@@ -852,6 +874,13 @@ cp4d30_modules() {
 
 }
 
+# writes IAF module data
+# updates the values across the respective workspace_configuration values
+iaf_modules() {
+    sleep 1
+
+}
+
 # Sets cluster id to user input or null
 # user input will use existing cluster for cloud pak install, null will create new cluster.
 get_cluster_info() {
@@ -1332,6 +1361,9 @@ if ! $EXISTING_CLUSTER
     then select_region
 fi
 
+if $IAF
+    then iaf_modules
+fi
 
 if $CP4MCM
     then cp4mcm_modules
