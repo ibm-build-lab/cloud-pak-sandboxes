4fc735f203726cfd3a5965b2fbffbbe436207244
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-05-24 15:52:13 -0500

    Update main.tf

--- terraform/cp4mcm/main.tf
+++ terraform/cp4mcm/main.tf
@@ -56,39 +56,6 @@ data "ibm_container_cluster_config" "cluster_config" {
   network           = false
 }
 
-module "portworx" {
-  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//portworx"
-  // TODO: With Terraform 0.13 replace the parameter 'enable' or the conditional expression using 'with_iaf' with 'count'
-  enable = var.install_portworx
-
-  ibmcloud_api_key = var.ibmcloud_api_key
-
-  // Cluster parameters
-  kube_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
-  worker_nodes     = var.workers_count[0]  // Number of workers
-
-  // Storage parameters
-  install_storage      = true
-  storage_capacity     = var.storage_capacity  // In GBs
-  storage_iops         = var.storage_iops   // Must be a number, it will not be used unless a storage_profile is set to a custom profile
-  storage_profile      = var.storage_profile
-
-  // Portworx parameters
-  resource_group_name   = var.resource_group
-  region                = var.region
-  cluster_id            = data.ibm_container_cluster_config.cluster_config.cluster_name_id
-  unique_id             = "px-roks"
-
-  // These credentials have been hard-coded because the 'Databases for etcd' service instance is not configured to have a publicly accessible endpoint by default.
-  // You may override these for additional security.
-  create_external_etcd  = var.create_external_etcd
-  etcd_username         = var.etcd_username
-  etcd_password         = var.etcd_password
-
-  // Defaulted.  Do not change
-  etcd_secret_name      = "px-etcd-certs"
-}
-
 // TODO: With Terraform 0.13 replace the parameter 'enable' with 'count'
 module "cp4mcm" {
   // source = "../../../../ibm-hcbt/terraform-ibm-cloud-pak/cp4mcm"
@@ -100,9 +67,6 @@ module "cp4mcm" {
   // ROKS cluster parameters:
   openshift_version   = local.roks_version
   cluster_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
-    
-  // Was Portworx installed? If so, will need to wait until it is finished
-  portworx_is_ready = var.install_portworx ? module.portworx.portworx_is_ready : "true"
 
   // Entitled Registry parameters:
   entitled_registry_key        = length(var.entitled_registry_key) > 0 ? var.entitled_registry_key : file(local.entitled_registry_key_file)
