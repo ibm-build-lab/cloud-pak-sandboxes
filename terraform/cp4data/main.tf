0f918ed2b5c1098b4433dbb7fdc1d2c1fb68f5bf
Eric Cambel <eric.cambel@ibm.com>
2021-05-27 10:21:30 -0400

    Created end-to-end for roks-px-cp4d

--- terraform/cp4data/main.tf
+++ terraform/cp4data/main.tf
@@ -1,6 +1,6 @@
 provider "ibm" {
-  generation = local.infra == "classic" ? 1 : 2
-  region     = var.region
+  region           = var.region
+  ibmcloud_api_key = var.ibmcloud_api_key
 }
 
 locals {
@@ -8,40 +8,42 @@ locals {
 }
 
 module "cluster" {
-  // source = "../../../../ibm-hcbt/terraform-ibm-cloud-pak/roks"
   source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//roks"
   enable = local.enable_cluster
-  on_vpc = local.infra == "vpc"
+  on_vpc = var.on_vpc
 
-  // General parameters:
-  project_name = var.project_name
-  owner        = var.owner
-  environment  = var.environment
+  // General
+  project_name   = var.project_name
+  owner          = var.owner
+  environment    = var.environment
+  resource_group = var.resource_group
+  roks_version   = var.roks_version
+  entitlement    = var.entitlement
+  force_delete_storage = var.force_delete_storage
 
-  // Openshift parameters:
-  resource_group       = var.resource_group
-  roks_version         = local.roks_version
-  flavors              = local.flavors
-  workers_count        = local.workers_count
-  datacenter           = var.datacenter
-  force_delete_storage = true
-
-  // Kubernetes Config parameters:
-  // download_config = false
-  // config_dir      = local.kubeconfig_dir
-  // config_admin    = false
-  // config_network  = false
-
-  // Debugging
+  // Parameters for the Workers
+  flavors        = var.flavors
+  workers_count  = var.workers_count
+  // Classic only
+  datacenter     = var.datacenter
   private_vlan_number = var.private_vlan_number
   public_vlan_number  = var.public_vlan_number
+  // VPC only
+  vpc_zone_names = var.vpc_zone_names
+
+  // Parameters for Kubernetes Config
+  // download_config = length(var.config_dir) > 0
+  // config_dir      = var.config_dir
+  // config_admin    = false
+  // config_network  = false
+  
 }
 
 resource "null_resource" "mkdir_kubeconfig_dir" {
   triggers = { always_run = timestamp() }
 
   provisioner "local-exec" {
-    command = "mkdir -p ${local.kubeconfig_dir}"
+    command = "mkdir -p ${var.config_dir}"
   }
 }
 
@@ -50,30 +52,70 @@ data "ibm_container_cluster_config" "cluster_config" {
 
   cluster_name_id   = local.enable_cluster ? module.cluster.id : var.cluster_id
   resource_group_id = module.cluster.resource_group.id
-  config_dir        = local.kubeconfig_dir
+  config_dir        = var.config_dir
   download          = true
   admin             = false
   network           = false
 }
 
+module "portworx" {
+  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//portworx"
+  // TODO: With Terraform 0.13 replace the parameter 'enable' or the conditional expression using 'with_iaf' with 'count'
+  enable = var.install_portworx
+
+  ibmcloud_api_key = var.ibmcloud_api_key
+
+  // Cluster parameters
+  kube_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
+  worker_nodes     = var.workers_count[0]  // Number of workers
+
+  // Storage parameters
+  install_storage      = true
+  storage_capacity     = var.storage_capacity  // In GBs
+  storage_iops         = var.storage_iops   // Must be a number, it will not be used unless a storage_profile is set to a custom profile
+  storage_profile      = var.storage_profile
+
+  // Portworx parameters
+  resource_group_name   = var.resource_group
+  region                = var.region
+  cluster_id            = data.ibm_container_cluster_config.cluster_config.cluster_name_id
+  unique_id             = "px-roks-${data.ibm_container_cluster_config.cluster_config.cluster_name_id}"
+
+  // These credentials have been hard-coded because the 'Databases for etcd' service instance is not configured to have a publicly accessible endpoint by default.
+  // You may override these for additional security.
+  create_external_etcd  = var.create_external_etcd
+  etcd_username         = var.etcd_username
+  etcd_password         = var.etcd_password
+
+  // Defaulted.  Don't change
+  etcd_secret_name      = "px-etcd-certs"
+}
+
 // TODO: With Terraform 0.13 replace the parameter 'enable' with 'count'
 module "cp4data" {
-  // source = "../../../../ibm-hcbt/terraform-ibm-cloud-pak/cp4data"
-  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//cp4data"
+  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//cp4data?ref=updateCp4d"
   enable = true
-  force  = true
-
 
   // ROKS cluster parameters:
-  openshift_version   = local.roks_version
+  openshift_version   = var.roks_version
   cluster_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
+  on_vpc              = var.on_vpc
+  portworx_is_ready   = module.portworx.portworx_is_ready // only need if on_vpc = true
+
+  // Prereqs
+  worker_node_flavor = var.flavors[0]
 
   // Entitled Registry parameters:
-  entitled_registry_key        = length(var.entitled_registry_key) > 0 ? var.entitled_registry_key : file(local.entitled_registry_key_file)
+  entitled_registry_key        = var.entitled_registry_key
   entitled_registry_user_email = var.entitled_registry_user_email
 
-  // Parameters to install CPD modules
-  empty_module_list                = var.empty_module_list
+  // CP4D License Acceptance
+  accept_cpd_license = var.accept_cpd_license
+
+  // CP4D Info
+  cpd_project_name = var.cpd_project_name
+
+  // Parameters to install submodules
   install_watson_knowledge_catalog = var.install_watson_knowledge_catalog
   install_watson_studio            = var.install_watson_studio
   install_watson_machine_learning  = var.install_watson_machine_learning
@@ -84,6 +126,7 @@ module "cp4data" {
   install_spark                    = var.install_spark
   install_db2_warehouse            = var.install_db2_warehouse
   install_db2_data_gate            = var.install_db2_data_gate
+  install_big_sql                  = var.install_big_sql
   install_rstudio                  = var.install_rstudio
   install_db2_data_management      = var.install_db2_data_management
 }
