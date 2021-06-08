9a7905499dcd8a078f33a03ffca89e70cc37ec65
Eric Cambel <eric.cambel@ibm.com>
2021-06-08 13:47:46 -0400

    updated source

--- terraform/cp4i/main.tf
+++ terraform/cp4i/main.tf
@@ -61,7 +61,7 @@ data "ibm_container_cluster_config" "cluster_config" {
 module "cp4i" {
   // source = "../../../../ibm-hcbt/terraform-ibm-cloud-pak/cp4data"
   # source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//cp4i"
-  source = "https://github.com/ibm-hcbt/terraform-ibm-cloud-pak/tree/cloudPakOutputs/cp4i"
+  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//cp4i?ref=cloudPakOutputs"
   enable = true
   force  = true
 
