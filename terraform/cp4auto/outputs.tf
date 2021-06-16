6cba64a518796810b9a91b45fb475055cec841f0
Joel Goddot <joel.goddot@gmail.com>
2021-06-16 14:52:22 -0400

    Update outputs.tf

--- terraform/cp4auto/outputs.tf
+++ terraform/cp4auto/outputs.tf
@@ -17,24 +17,24 @@ output "kubeconfig" {
   value = data.ibm_container_cluster_config.cluster_config.config_file_path
 }
 
-output "namespace" {
-  value = var.namespace
-}
+//output "namespace" {
+//  value = var.namespace
+//}
 
-output "cp4ba_url" {
-  description = "Use this URL to access your Cloud Pak for Business Automation"
-  value       = module.cp4auto.cp4auto_endpoint
-}
+//output "cp4ba_url" {
+//  description = "Use this URL to access your Cloud Pak for Business Automation"
+//  value       = module.cp4auto.cp4auto_endpoint
+//}
 
 output "cp4ba_username" {
   description = "Username for your Cloud Pak for Business Automation"
   value = module.cp4auto.cp4auto_username
 }
 
-//output "cp4ba_password" {
-//  description = "Password for your Cloud Pak Integration deployment"
-//  value = module.cp4auto.cp4auto_password
-//}
+output "cp4ba_password" {
+  description = "Password for your Cloud Pak Integration deployment"
+  value = module.cp4auto.cp4auto_password
+}
 
 // output "cluster_config" {
 //   value = module.cluster.config
