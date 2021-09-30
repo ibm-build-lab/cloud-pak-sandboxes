3da388841eee8388933eb97e8c1af45c04853e4a
Joel Goddot <joel.goddot@gmail.com>
2021-09-30 11:56:28 -0400

    Updating variables

--- terraform/cp4ba/main.tf
+++ terraform/cp4ba/main.tf
@@ -5,9 +5,6 @@ provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
 }
 
-
-// user output and pwd outputs
-
 # Getting the OpenShift cluster configuration
 data "ibm_resource_group" "group" {
   name = var.resource_group
@@ -20,21 +17,9 @@ resource "null_resource" "mkdir_kubeconfig_dir" {
   }
 }
 
-data "ibm_container_cluster_config" "cluster_config" {
-  depends_on = [null_resource.mkdir_kubeconfig_dir]
-  cluster_name_id   = var.cluster_name_id
-  resource_group_id = data.ibm_resource_group.group.id
-  download          = true
-  config_dir        = "./kube/config"
-  admin             = false
-  network           = false
-}
-
 
 module "cluster" {
   source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak/tree/main/modules/roks"
-//  source = "../../roks"
-  // source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//modules/roks"
   enable = local.enable_cluster
 //  on_vpc = var.on_vpc
 
@@ -62,109 +47,73 @@ resource "null_resource" "mkdir_kubeconfig_dir" {
   }
 }
 
-//////######################### LDAP ##################################
-module "ldap" {
-  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak/tree/main/modules/ldap"
-//  source = "../../ldap"
-//  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//modules/ldap"
-
-  ibmcloud_api_key      = var.ibmcloud_api_key
-  region                = var.region
-  iaas_classic_api_key  = var.iaas_classic_api_key
-  iaas_classic_username = var.iaas_classic_username
-  ssh_public_key_file   = var.ssh_public_key_file
-  ssh_private_key_file  = var.ssh_private_key_file
-  classic_datacenter    = var.classic_datacenter
+data "ibm_container_cluster_config" "cluster_config" {
+  depends_on = [null_resource.mkdir_kubeconfig_dir]
+  cluster_name_id   = var.cluster_name_id
+  resource_group_id = data.ibm_resource_group.group.id
+  download          = true
+  config_dir        = "./kube/config"
+  admin             = false
+  network           = false
 }
 
-module "cp4ba"{
-    source = "git::https://github.com/jgod1360/terraform-ibm-cloud-pak/tree/cp4ba/modules/cp4ba"
-//    source = "../.."
-    // TODO: With Terraform 0.13 replace the parameter 'enable' or the conditional expression using 'with_iaf' with 'count'
-    enable = true
+# --------------- PROVISION DB2  ------------------
+module "install_db2" {
+  source = "/bin/bash ./deployment-db2-cp4ba/03_create_Db2_on_OCP.sh"
 
-//    openshift_version   = var.openshift_version
-//    cluster_config_path = data.ibm_container_cluster_config.cluster_config.config_dir
-//    cluster_name_id     = var.cluster_name_id
-//    on_vpc              = var.on_vpc
+  //  environment {
+  # CP4BA Database Name information
 
-    // IBM Cloud API Key
-     CLUSTER_NAME_OR_ID     = var.cluster_name_or_id
-    //    on_vpc              = var.on_vpc
-
-        // IBM Cloud API Key
-      IBMCLOUD_API_KEY = var.ibmcloud_api_key
-
-      # Cluster
-  //    on_vpc                        = var.on_vpc
-  //    portworx_is_ready             = var.portworx_is_ready
-  //    namespace                     = local.cp4ba_namespace
-  //
-  //    # Platform
-      PLATFORM_SELECTED              = local.platform_options
-      PLATFORM_VERSION              = local.platform_version
-      PROJECT_NAME                     = local.project_name
-      DEPLOYMENT_TYPE               = local.deployment_type
-      USER_NAME_EMAIL                = var.entitled_registry_user_email
-      USE_ENTITLEMENT               = local.use_entitlement
-      ENTITLED_REGISTRY_KEY               = var.entitlement_key # file("${path.cwd}/../../entitlement.key")
-      # Registry Images
-      DOCKER_SECRET_NAME            = var.docker_secret_name
-      DOCKER_SERVER                 = local.docker_server
-      DOCKER_USERNAME               = local.docker_username
-      DOCKER_REGISTRY_PASS               = local.docker_password
-      DOCKER_USER_EMAIL                  = local.docker_email
-      public_registry_server        = var.public_registry_server
-      LOCAL_PUBLIC_REGISTRY_SERVER   = var.public_image_registry
-  //    local_registry_server         = var.registry_server
-  //    local_registry_user           = var.registry_user
-
-  //    # Storage Classes
-      STORAGE_CLASSNAME            = local.storage_class_name
-      SC_SLOW_FILE_STORAGE_CLASSNAME   = local.sc_slow_file_storage_classname
-      SC_MEDIUM_FILE_STORAGE_CLASSNAME = local.sc_medium_file_storage_classname
-      SC_FAST_FILE_STORAGE_CLASSNAME   = local.sc_fast_file_storage_classname
+  # ************** Hardcode all these variables ****************
+  DB2_UMS_DB_NAME = local.db2_ums_db_name
+  DB2_ICN_DB_NAME = local.db2_icn_db_name
+  DB2_DEVOS_1_NAME = local.db2_devos_1_name
+  DB2_AEOS_NAME = local.db2_aeos_name
+  DB2_BAW_DOCS_NAME = local.db2_baw_docs_name
+  DB2_BAW_TOS_NAME = local.db2_baw_tos_name
+  DB2_BAW_DOS_NAME = local.db2_baw_dos_name
+  DB2_BAW_DB_NAME = local.db2_baw_Db_name
+  DB2_APP_DB_NAME = local.db2_app_db_name
+  DB2_AE_DB_NAME = local.db2_ae_db_name
+  DB2_BAS_DB_NAME = local.db2_bas_db_name
+  DB2_GCD_DB_NAME = local.db2_gcd_db_name
+  DB2_ON_OCP_PROJECT_NAME = local.db2_project_name
+  DB2_ADMIN_USER_NAME = local.db2_admin_user_name
+  DB2_ADMIN_USER_PASSWORD = local.db2_admin_user_password
+  DB2_STANDARD_LICENSE_KEY = local.db2_standard_license_key
+  DB2_CPU = local.db2_cpu
+  DB2_MEMORY = local.db2_memory
+  DB2_INSTANCE_VERSION = local.db2_instance_version
+  DB2_HOST_NAME = local.db2_host_name
+  //    DB2_HOST_IP              = local.db2_host_ip
+  //    DB2_PORT_NUMBER          = local.db2_port_number
+  //    DB2_USE_ONN_OCP          = local.db2_use_on_ocp
+  //    CP4BA_DEPLOYMENT_PLATFORM = local.cp4ba_deployment_platform
+  DB2_ON_OCP_STORAGE_CLASS_NAME = local.db2_on_ocp_storage_class_name
+  DB2_STORAGE_SIZE = local.db2_storage_size
+  # ------ Docker Information ----------
+  ENTITLED_REGISTRY_KEY = var.entitlement_key
+  ENTITLEMENT_REGISTRY_USER_EMAIL = var.entitled_registry_user_email
+  # Entilement_user
+  DOCKER_SERVER = local.docker_server
+  DOCKER_USERNAME = local.docker_username
 }
 
-////######################### DB2 ##################################
-data "external" "install_db2" {
-
-  depends_on = [
-    module.cp4ba
-  ]
-
-  program = [
-    "/bin/bash", "https://github.com/jgod1360/terraform-ibm-cloud-pak/blob/cp4ba/modules/cp4ba/scripts/install_db2_local.sh"]
-//    "/bin/bash", "../../scripts/install_db2_local.sh"]
-
-  query = {
-    kubeconfig = var.cluster_config_path
-    namespace = var.namespace
-  }
-}
-
-
-######################### PORTWORX ##################################
 module "portworx" {
-    depends_on = [
-    module.cp4ba
-  ]
-
-  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak/tree/main/modules/portworx"
-//  source = "../../portworx"
-//  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//modules/portworx"
-  // TODO: With Terraform 0.13 replace the parameter 'enable' or the conditional expression using with 'count'
+  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//modules/portworx"
+  // TODO: With Terraform 0.13 replace the parameter 'enable' or the conditional expression using 'with_iaf' with 'count'
   enable = var.install_portworx
 
   ibmcloud_api_key = var.ibmcloud_api_key
 
   // Cluster parameters
-  kube_config_path = data.ibm_container_cluster_config.cluster_config.config_dir
+  kube_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
   worker_nodes     = var.workers_count[0]  // Number of workers
 
   // Storage parameters
   install_storage      = true
   storage_capacity     = var.storage_capacity  // In GBs
+  storage_iops         = var.storage_iops   // Must be a number, it will not be used unless a storage_profile is set to a custom profile
   storage_profile      = var.storage_profile
 
   // Portworx parameters
@@ -182,6 +131,61 @@ module "portworx" {
   // Defaulted.  Don't change
   etcd_secret_name      = "px-etcd-certs"
 }
+
+
+module "cp4ba"{
+    source = "git::https://github.com/jgod1360/terraform-ibm-cloud-pak/tree/cp4ba/modules/cp4ba"
+
+  CLUSTER_NAME_OR_ID     = var.cluster_name_or_id
+
+  # ---- IBM Cloud API Key ----
+  IBMCLOUD_API_KEY = var.ibmcloud_api_key
+
+  # ---- Platform ----
+  CP4BA_PROJECT_NAME            = var.cp4ba_project_name
+  USER_NAME_EMAIL               = var.entitled_registry_user_email
+  USE_ENTITLEMENT               = local.use_entitlement
+  ENTITLED_REGISTRY_KEY         = var.entitlement_key
+
+  # ---- Registry Images ----
+  ENTITLED_REGISTRY_KEY_SECRET_NAME = local.entitled_registry_key_secret_name
+  DOCKER_SERVER                 = local.docker_server
+  DOCKER_USERNAME               = local.docker_username
+  DOCKER_USER_EMAIL             = local.docker_email
+
+  # ------- FILES ASSIGNMENTS --------
+//  OPERATOR_PVC_FILE                = local.pvc_file
+//  CATALOG_SOURCE_FILE              = local.catalog_source_file
+//  IBM_CP4BA_CRD_FILE               = local.ibm_cp4ba_crd_file
+//  IBM_CP4BA_CR_FINAL_TMPL_FILE     = local.ibm_cp4ba_cr_final_tmpl_file
+//  CP4BA_SUBSCRIPTION_FILE          = local.cp4ba_subscription_file
+  CP4BA_ADMIN_NAME                 = local.cp4ba_admin_name
+  CP4BA_ADMIN_GROUP                = local.cp4ba_admin_group
+  CP4BA_USERS_GROUP                = local.cp4ba_users_group
+  CP4BA_UMS_ADMIN_NAME             = local.cp4ba_ums_admin_name
+  CP4BA_UMS_ADMIN_GROUP            = local.cp4ba_ums_admin_group
+//  CP4BA_ADMIN_PASSWORD             = var.cp4ba_admin_password
+//  CP4BA_UMS_ADMIN_PASSWORD         = var.cp4ba_ums_admin_password
+
+  # ---- Storage Classes ----
+  SC_SLOW_FILE_STORAGE_CLASSNAME   = local.sc_slow_file_storage_classname
+  SC_MEDIUM_FILE_STORAGE_CLASSNAME = local.sc_medium_file_storage_classname
+  SC_FAST_FILE_STORAGE_CLASSNAME   = local.sc_fast_file_storage_classname
+
+  # ----- DB2 Settings -----
+  DB2_PORT_NUMBER         = var.db2_port_number
+  DB2_HOST_NAME           = var.db2_host_name
+  DB2_HOST_IP             = var.db2_host_ip
+  DB2_ADMIN_USERNAME      = var.db2_admin_username
+  DB2_ADMIN_USER_PASSWORD = var.db2_admin_user_password
+
+  # ----- LDAP Settings -----
+  LDAP_ADMIN_NAME         = local.ldap_admin_name
+  LDAP_ADMIN_PASSWORD     = var.ldap_admin_password
+}
+
+
+
 
 
 
--- terraform/cp4ba/variables.tf
+++ terraform/cp4ba/variables.tf
@@ -19,8 +19,8 @@ variable "entitled_registry_user_email" {
 
 variable "iaas_classic_api_key" {}
 variable "iaas_classic_username" {}
-variable "ssh_public_key_file" {}
-variable "ssh_private_key_file" {}
+//variable "ssh_public_key_file" {}
+//variable "ssh_private_key_file" {}
 variable "classic_datacenter" {}
 
 variable "config_dir" {
@@ -101,31 +101,31 @@ variable "use_entitlement" {
   default = "yes"
 }
 
-variable "public_image_registry" {
-  description = "Have you pushed the images to the local registry using 'loadimages.sh' (CP4BA images)? If not, Please pull the images to the local images to proceed."
-}
-
-variable "local_public_registry_server" {
-  description = "public image registry or route for docker/podman login validation: \n (e.g., default-route-openshift-image-registry.apps.<hostname>). This is required for docker/podman login validation: "
-}
-
-variable "local_registry_user" {
-  description = "Enter the user name for your docker registry: "
-}
-
-variable "local_registry_password" {
-  description = "Enter the password for your docker registry: "
-}
-
-//1
-
-// Flavor will depend on whether classic or vpc
-variable "flavors" {
-  type        = list(string)
-  default     = ["b3c.16x64"]
-  description = "Ignored if `cluster_id` is specified. Array with the flavors or machine types of each the workers group. Classic only takes the first flavor of the list. List all flavors for each zone with: `ibmcloud ks flavors --zone us-south-1 --provider <classic | vpc-gen2>`. Classic: `[\"b3c.16x64\"]`, VPC: `[\"bx2.16x64\"]`"
-}
-
+//variable "public_image_registry" {
+//  description = "Have you pushed the images to the local registry using 'loadimages.sh' (CP4BA images)? If not, Please pull the images to the local images to proceed."
+//}
+//
+//variable "local_public_registry_server" {
+//  description = "public image registry or route for docker/podman login validation: \n (e.g., default-route-openshift-image-registry.apps.<hostname>). This is required for docker/podman login validation: "
+//}
+//
+//variable "local_registry_user" {
+//  description = "Enter the user name for your docker registry: "
+//}
+//
+//variable "local_registry_password" {
+//  description = "Enter the password for your docker registry: "
+//}
+//
+////1
+//
+//// Flavor will depend on whether classic or vpc
+//variable "flavors" {
+//  type        = list(string)
+//  default     = ["b3c.16x64"]
+//  description = "Ignored if `cluster_id` is specified. Array with the flavors or machine types of each the workers group. Classic only takes the first flavor of the list. List all flavors for each zone with: `ibmcloud ks flavors --zone us-south-1 --provider <classic | vpc-gen2>`. Classic: `[\"b3c.16x64\"]`, VPC: `[\"bx2.16x64\"]`"
+//}
+//
 variable "workers_count" {
   type    = list(number)
   default = [5]
@@ -153,41 +153,41 @@ variable "public_vlan_number" {
   description = "**Classic Only**. Ignored if `cluster_id` is specified. Public VLAN assigned to your zone. List available VLANs in the zone: `ibmcloud ks vlan ls --zone <zone>`, make sure the the VLAN type is public and the router begins with fc. Use the ID or Number. Leave blank if Public VLAN does not exist, one will be created"
 }
 
-variable "storage_capacity"{
-    type = number
-    default = 200
-    description = "Ignored if Portworx is not enabled: Storage capacity in GBs"
-}
-
-variable "storage_db2" {
-    type = number
-    default = 10
-    description = "Ignored if Portworx is not enabled. Optional, Used only if a user provides a custom storage_profile"
-}
-
-variable "storage_profile" {
-    type = string
-    default = "10iops-tier"
-    description = "Ignored if Portworx is not enabled. Optional, Storage profile used for creating storage"
-}
-
-variable "create_external_etcd" {
-    type = bool
-    default = false
-    description = "Ignored if Portworx is not enabled: Do you want to create an external etcd database? `true` or `false`"
-}
-
-# These credentials have been hard-coded because the 'Databases for etcd' service instance is not configured to have a publicly accessible endpoint by default.
-# You may override these for additional security.
-variable "etcd_username" {
-  default = ""
-  description = "Ignored if Portworx is not enabled: This has been hard-coded because the 'Databases for etcd' service instance is not configured to have a publicly accessible endpoint by default.  Override these for additional security."
-}
-
-variable "etcd_password" {
-  default = ""
-  description = "Ignored if Portworx is not enabled: This has been hard-coded because the 'Databases for etcd' service instance is not configured to have a publicly accessible endpoint by default.  Override these for additional security."
-}
+//variable "storage_capacity"{
+//    type = number
+//    default = 200
+//    description = "Ignored if Portworx is not enabled: Storage capacity in GBs"
+//}
+//
+//variable "storage_db2" {
+//    type = number
+//    default = 10
+//    description = "Ignored if Portworx is not enabled. Optional, Used only if a user provides a custom storage_profile"
+//}
+//
+//variable "storage_profile" {
+//    type = string
+//    default = "10iops-tier"
+//    description = "Ignored if Portworx is not enabled. Optional, Storage profile used for creating storage"
+//}
+//
+//variable "create_external_etcd" {
+//    type = bool
+//    default = false
+//    description = "Ignored if Portworx is not enabled: Do you want to create an external etcd database? `true` or `false`"
+//}
+//
+//# These credentials have been hard-coded because the 'Databases for etcd' service instance is not configured to have a publicly accessible endpoint by default.
+//# You may override these for additional security.
+//variable "etcd_username" {
+//  default = ""
+//  description = "Ignored if Portworx is not enabled: This has been hard-coded because the 'Databases for etcd' service instance is not configured to have a publicly accessible endpoint by default.  Override these for additional security."
+//}
+//
+//variable "etcd_password" {
+//  default = ""
+//  description = "Ignored if Portworx is not enabled: This has been hard-coded because the 'Databases for etcd' service instance is not configured to have a publicly accessible endpoint by default.  Override these for additional security."
+//}
 
 variable "cluster_config_path" {
   default     = "./.kube/config"
@@ -201,10 +201,6 @@ variable "registry_server" {
 variable "entitlement_key" {
   type        = string
   description = "Do you have a Cloud Pak for Business Automation Entitlement Registry key? If not, Get the entitlement key from https://myibm.ibm.com/products-services/containerlibrary"
-}
-
-variable "public_registry_server" {
-  description = "public image registry or route for docker/podman login validation: \n (e.g., default-route-openshift-image-registry.apps.<hostname>). This is required for docker/podman login validation: "
 }
 
 variable "registry_user" {
@@ -228,18 +224,103 @@ variable "cluster_name_or_id" {
   description = "Enter your cluster id or name to install the Cloud Pak. Leave blank to provision a new Openshift cluster."
 }
 
+variable "cp4ba_project_name" {
+  default = "cp4ba"
+  description = "Project name or namespace where Cloud Pak for Business Automation will be installed."
+}
 
-//variable "cluster_id" {
-//  default     = ""
-//  description = "Enter your cluster id or name to install the Cloud Pak. Leave blank to provision a new Openshift cluster."
-//}
-
-// Portworx Module Variables
 variable "install_portworx" {
   type        = bool
   default     = false
   description = "Install Portworx on the ROKS cluster. `true` or `false`"
 }
+
+variable "storage_capacity"{
+    type = number
+    default = 200
+    description = "Ignored if Portworx is not enabled: Storage capacityin GBs"
+}
+
+variable "storage_profile" {
+    type = string
+    default = "10iops-tier"
+    description = "Ignored if Portworx is not enabled. Optional, Storage profile used for creating storage"
+}
+
+variable "storage_iops" {
+    type = number
+    default = 10
+    description = "Ignored if Portworx is not enabled. Optional, Used only if a user provides a custom storage_profile"
+}
+
+variable "create_external_etcd" {
+    type = bool
+    default = false
+    description = "Ignored if Portworx is not enabled: Do you want to create an external etcd database? `true` or `false`"
+}
+
+# These credentials have been hard-coded because the 'Databases for etcd' service instance is not configured to have a publicly accessible endpoint by default.
+# You may override these for additional security.
+variable "etcd_username" {
+  default = ""
+  description = "Ignored if Portworx is not enabled: This has been hard-coded because the 'Databases for etcd' service instance is not configured to have a publicly accessible endpoint by default.  Override these for additional security."
+}
+
+variable "etcd_password" {
+  default = ""
+  description = "Ignored if Portworx is not enabled: This has been hard-coded because the 'Databases for etcd' service instance is not configured to have a publicly accessible endpoint by default.  Override these for additional security."
+}
+
+// OpenShift cluster specific input parameters and default values:
+variable "flavors" {
+  type    = list(string)
+  default = ["b3c.16x64"]
+  description = "Ignored if `cluster_id` is specified. Array with the flavors or machine types of each of the workers. List all flavors for each zone with: `ibmcloud ks flavors --zone us-south-1 --provider vpc-gen2` or `ibmcloud ks flavors --zone dal10 --provider classic`. On Classic only list one flavor, i.e. `[\"b3c.16x64\"]`. On VPC can list multiple flavors `[\"mx2.4x32\", \"mx2.8x64\", \"cx2.4x8\"] or [\"bx2.16x64\"]`"
+}
+
+# Password for LDAP Admin User (ldapAdminName name see below), for example passw0rd - use the password that you specified when setting up LDAP
+variable "ldap_admin_password" {}
+
+# LDAP instance access information - hostname or IP
+variable "ldap_server" {}
+
+# --------- DB2 SETTINGS ----------
+locals {
+  # CP4BA Database Name information
+  db2_ums_db_name   = "UMSDB"
+  db2_icn_db_name   = "ICNDB"
+  db2_devos_1_name  = "DEVOS1"
+  db2_aeos_name     = "AEOS"
+  db2_baw_docs_name = "BAWDOCS"
+  db2_baw_tos_name  = "BAWTOS"
+  db2_baw_dos_name  = "BAWDOS"
+  db2_baw_Db_name   = "BAWDB"
+  db2_app_db_name   = "APPDB"
+  db2_ae_db_name    = "AEDB"
+  db2_bas_db_name   = "BASDB"
+  db2_gcd_db_name   = "GCDDB"
+  db2_admin_user_password  = "passw0rd"
+  db2_standard_license_key = "W0xpY2Vuc2VDZXJ0aWZpY2F0ZV0KQ2hlY2tTdW09Q0FBODlCOTA0QzU3RTY2OTU1RjJDQTY4MzlCRTZCOTMKVGltZVN0YW1wPTE1NjU3MjM5MDIKUGFzc3dvcmRWZXJzaW9uPTQKVmVuZG9yTmFtZT1JQk0gVG9yb250byBMYWIKVmVuZG9yUGFzc3dvcmQ9N3Y4cDRmcTJkdGZwYwpWZW5kb3JJRD01ZmJlZTBlZTZmZWIuMDIuMDkuMTUuMGYuNDguMDAuMDAuMDAKUHJvZHVjdE5hbWU9REIyIFN0YW5kYXJkIEVkaXRpb24KUHJvZHVjdElEPTE0MDUKUHJvZHVjdFZlcnNpb249MTEuNQpQcm9kdWN0UGFzc3dvcmQ9MzR2cnc1MmQyYmQyNGd0NWFmNHU4Y2M0ClByb2R1Y3RBbm5vdGF0aW9uPTEyNyAxNDMgMjU1IDI1NSA5NCAyNTUgMSAwIDAgMC0yNzsjMCAxMjggMTYgMCAwCkFkZGl0aW9uYWxMaWNlbnNlRGF0YT0KTGljZW5zZVN0eWxlPW5vZGVsb2NrZWQKTGljZW5zZVN0YXJ0RGF0ZT0wOC8xMy8yMDE5CkxpY2Vuc2VEdXJhdGlvbj02NzE2CkxpY2Vuc2VFbmREYXRlPTEyLzMxLzIwMzcKTGljZW5zZUNvdW50PTEKTXVsdGlVc2VSdWxlcz0KUmVnaXN0cmF0aW9uTGV2ZWw9MwpUcnlBbmRCdXk9Tm8KU29mdFN0b3A9Tm8KQnVuZGxlPU5vCkN1c3RvbUF0dHJpYnV0ZTE9Tm8KQ3VzdG9tQXR0cmlidXRlMj1ObwpDdXN0b21BdHRyaWJ1dGUzPU5vClN1YkNhcGFjaXR5RWxpZ2libGVQcm9kdWN0PU5vClRhcmdldFR5cGU9QU5ZClRhcmdldFR5cGVOYW1lPU9wZW4gVGFyZ2V0ClRhcmdldElEPUFOWQpFeHRlbmRlZFRhcmdldFR5cGU9CkV4dGVuZGVkVGFyZ2V0SUQ9ClNlcmlhbE51bWJlcj0KVXBncmFkZT1ObwpJbnN0YWxsUHJvZ3JhbT0KQ2FwYWNpdHlUeXBlPQpNYXhPZmZsaW5lUGVyaW9kPQpEZXJpdmVkTGljZW5zZVN0eWxlPQpEZXJpdmVkTGljZW5zZVN0YXJ0RGF0ZT0KRGVyaXZlZExpY2Vuc2VFbmREYXRlPQpEZXJpdmVkTGljZW5zZUFnZ3JlZ2F0ZUR1cmF0aW9uPQo="
+  db2_cpu    = 4
+  db2_memory = "16Gi"
+  db2_instance_version = "11.5.6.0"
+  db2_host_name   = "REQUIRED"
+  db2_host_ip     = "REQUIRED"
+  db2_port_number = "REQUIRED"
+  db2_use_on_ocp  = true
+  db2_admin_user_name           = "db2inst1"
+//  cp4ba_deployment_platform     = local.platform_options
+  db2_on_ocp_storage_class_name = local.sc_fast_file_storage_classname
+  db2_storage_size              = "150Gi"
+  db2_project_name              = "ibm-db2"
+}
+
+// Portworx Module Variables
+//variable "install_portworx" {
+//  type        = bool
+//  default     = false
+//  description = "Install Portworx on the ROKS cluster. `true` or `false`"
+//}
 
 locals {
 //  cp4ba_namespace              = "cp4ba"
@@ -260,27 +341,76 @@ locals {
 //  entitled_registry_key        = chomp(var.entitlement_key)
   ibmcloud_api_key             = chomp(var.ibmcloud_api_key)
  }
-//
-//locals {
-//  cp4ba_namespace              = "cp4ba-project"
-//  entitled_registry            = "cp.icr.io"
-//  entitled_registry_user       = "cp"
-//  enable_cluster               = var.cluster_name_id == "" || var.cluster_name_id == null
-//  use_entitlement              = var.use_entitlement #? ["yes", "Yes", "YES", "y", "Y"] : ["", "n", "N", "no", "No", "NO"]
-//  local_public_registry_server = var.local_public_registry_server
-//  local_public_image_registry  = var.public_image_registry
-//  local_registry_server        = var.local_registry_server
-//  local_registry_user          = var.local_registry_user
-//  local_registry_password      = var.local_registry_password
-//  entitled_registry_key        = chomp(var.entitled_registry_key)
-//  ibmcloud_api_key             = chomp(var.ibmcloud_api_key)
-//  openshift_version_regex      = regex("(\\d+).(\\d+)(.\\d+)*(_openshift)*", var.openshift_version)
-//  openshift_version_number     = local.openshift_version_regex[3] == "_openshift" ? tonumber("${local.openshift_version_regex[0]}.${local.openshift_version_regex[1]}") : 0
-//}
 
 locals {
   storage_class_name               = "cp4a-file-retain-gold-gid"
   sc_slow_file_storage_classname   = "cp4a-file-retain-bronze-gid"
   sc_medium_file_storage_classname = "cp4a-file-retain-silver-gid"
   sc_fast_file_storage_classname   = "cp4a-file-retain-gold-gid"
+}
+
+
+# -------- DB2 Variables ---------
+variable "db2_admin_user_password" {
+  default = "passw0rd"
+}
+
+variable "db2_admin_username" {
+  default = "db2inst1"
+}
+
+variable "db2_host_name" {}
+
+variable "db2_host_ip" {}
+
+variable "db2_port_number" {}
+
+locals {
+  //  cp4ba_namespace              = "cp4ba"
+  entitled_registry_key_secret_name  = "ibm-entitlement-key"
+  docker_server                = "cp.icr.io"
+  docker_username              = "cp"
+  docker_email                 = var.entitled_registry_user_email
+  enable_cluster               = var.cluster_name_or_id == "" || var.cluster_name_or_id == null
+  use_entitlement              = "yes"
+  ibmcloud_api_key             = chomp(var.ibmcloud_api_key)
+}
+
+# --- CP4BA SETTINGS ---
+locals {
+  cp4ba_admin_name = "cp4badmin"
+  cp4ba_admin_group = "cp4badmins"
+  cp4ba_users_group = "cp4bausers"
+  cp4ba_ums_admin_name = "umsadmin"
+  cp4ba_ums_admin_group = "cn=cp4badmins,dc=example,dc=com"
+}
+
+# --- LDAP SETTINGS ---
+locals {
+  # LDAP name - don't use dashes (-), only use underscores
+  ldap_name = "ldap_custom"
+  ldap_admin_name = "cn=root"
+  ldap_type = "IBM Security Directory Server"
+  ldap_port = "389"
+  ldap_server = "150.238.92.26"
+  ldap_base_dn = "dc=example,dc=com"
+  ldap_user_name_attribute = "*:cn"
+  ldap_user_display_name_attr = "cn"
+  ldap_group_base_dn = "dc=example,dc=com"
+  ldap_group_name_attribute = "*:cn"
+  ldap_group_display_name_attr = "cn"
+  ldap_group_membership_search_filter = "('\\|(\\&(objectclass=groupOfNames)(member={0}))(\\&(objectclass=groupOfUniqueNames)(uniqueMember={0})))"
+  ldap_group_member_id_map = "groupofnames:member"
+  ldap_ad_gc_host = ""
+  ldap_ad_gc_port = ""
+  ldap_ad_user_filter = "(\\&(samAccountName=%v)(objectClass=user))"
+  ldap_ad_group_filter = "(\\&(samAccountName=%v)(objectclass=group))"
+  ldap_tds_user_filter = "(\\&(cn=%v)(objectclass=person))"
+  ldap_tds_group_filter = "(\\&(cn=%v)(\\|(objectclass=groupofnames)(objectclass=groupofuniquenames)(objectclass=groupofurls)))"
+}
+
+# --- HA Settings ---
+locals {
+  cp4ba_replica_count = 1
+  cp4ba_bai_job_parallelism = 1
 }
