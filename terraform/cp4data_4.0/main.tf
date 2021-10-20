b3cba912c803592c2739fcfd9a999b366c1b4968
Jeremy Menke <87037301+jeremymenke@users.noreply.github.com>
2021-10-20 13:39:42 -0400

    Cpd40 (#46)
    * Making base directories to start.
    
    * Bunch of install var updates for 4.0
    
    * cloud-pak-installer.sh added cp4d 4.0
    
    * Test 2 - point to cpd40 repo instead of master .
    
    * Missing vars in main.tf
    
    * Added missing variables cp4d.
    
    * Fixed output.
    
    * Increased sleep time for create, generate
    
    * Update resource_group_name
    
    * RG name updates
    
    * cloud_pak entitlement
    
    * Remove Cp4d 3.0
    
    * Remove Cp4d 3.0
    
    * Remove 3.0 update
    
    * Suggested cluster_id fix in main.tf
    
    * Workspace time waitings shortened.
    
    * cpd40 small change
    
    * Update cloud-pak-installer.sh
    
    * Update cloud-pak-installer.sh

---
+++ terraform/cp4data_4.0/main.tf
@@ -0,0 +1,144 @@
+provider "ibm" {
+  version    = "~> 1.12"
+  region           = var.region
+  ibmcloud_api_key = var.ibmcloud_api_key
+}
+
+locals {
+  enable_cluster = var.cluster_id == null || var.cluster_id == ""
+}
+
+module "cluster" {
+  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//modules/roks"
+  enable = local.enable_cluster
+  on_vpc = var.on_vpc
+
+  // General
+  project_name   = var.project_name
+  owner          = var.owner
+  environment    = var.environment
+  resource_group = var.resource_group
+  roks_version   = var.roks_version
+  entitlement    = var.entitlement
+  force_delete_storage = var.force_delete_storage
+
+  // Parameters for the Workers
+  flavors        = var.flavors
+  workers_count  = var.workers_count
+  // Classic only
+  datacenter     = var.datacenter
+  private_vlan_number = var.private_vlan_number
+  public_vlan_number  = var.public_vlan_number
+  // VPC only
+  vpc_zone_names = var.vpc_zone_names
+
+  // Parameters for Kubernetes Config
+  // download_config = length(var.config_dir) > 0
+  // config_dir      = var.config_dir
+  // config_admin    = false
+  // config_network  = false
+  
+}
+
+resource "null_resource" "mkdir_kubeconfig_dir" {
+  triggers = { always_run = timestamp() }
+
+  provisioner "local-exec" {
+    command = "mkdir -p ${var.config_dir}"
+  }
+}
+
+data "ibm_container_cluster_config" "cluster_config" {
+  depends_on = [null_resource.mkdir_kubeconfig_dir]
+
+  cluster_name_id   = local.enable_cluster ? module.cluster.id : var.cluster_id
+  resource_group_id = module.cluster.resource_group.id
+  config_dir        = var.config_dir
+  download          = true
+  admin             = false
+  network           = false
+}
+
+module "portworx" {
+  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//modules/portworx"
+  // TODO: With Terraform 0.13 replace the parameter 'enable' or the conditional expression using 'with_iaf' with 'count'
+  enable = var.install_portworx
+
+  ibmcloud_api_key = var.ibmcloud_api_key
+
+  // Cluster parameters
+  kube_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
+  worker_nodes     = var.workers_count[0]  // Number of workers
+
+  // Storage parameters
+  install_storage      = true
+  storage_capacity     = var.storage_capacity  // In GBs
+  storage_iops         = var.storage_iops   // Must be a number, it will not be used unless a storage_profile is set to a custom profile
+  storage_profile      = var.storage_profile
+
+  // Portworx parameters
+  resource_group_name   = var.resource_group
+  region                = var.region
+  cluster_id            = data.ibm_container_cluster_config.cluster_config.cluster_name_id
+  unique_id             = "px-roks-${data.ibm_container_cluster_config.cluster_config.cluster_name_id}"
+
+  // These credentials have been hard-coded because the 'Databases for etcd' service instance is not configured to have a publicly accessible endpoint by default.
+  // You may override these for additional security.
+  create_external_etcd  = var.create_external_etcd
+  etcd_username         = var.etcd_username
+  etcd_password         = var.etcd_password
+
+  // Defaulted.  Don't change
+  etcd_secret_name      = "px-etcd-certs"
+}
+
+// TODO: With Terraform 0.13 replace the parameter 'enable' with 'count'
+module "cp4data" {
+  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak.git//modules/cp4data_4.0"
+  enable = true
+
+  // ROKS cluster parameters:
+  openshift_version   = var.roks_version
+  cluster_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
+  on_vpc              = var.on_vpc
+  portworx_is_ready   = module.portworx.portworx_is_ready // only need if on_vpc = true
+
+  // Prereqs
+  worker_node_flavor = var.flavors[0]
+
+  operator_namespace = var.operator_namespace
+
+  // Entitled Registry parameters:
+  entitled_registry_key        = var.entitled_registry_key
+  entitled_registry_user_email = var.entitled_registry_user_email
+
+  // CP4D License Acceptance
+  accept_cpd_license = var.accept_cpd_license
+
+  // CP4D Info
+  cpd_project_name = var.cpd_project_name
+
+  // IBM Cloud API Key
+  ibmcloud_api_key          = var.ibmcloud_api_key
+
+  region = var.region
+  resource_group_name = var.resource_group
+  cluster_id = local.enable_cluster ? module.cluster.id : var.cluster_id
+
+  // Parameters to install submodules
+  install_wsl         = var.install_wsl
+  install_aiopenscale = var.install_aiopenscale
+  install_wml         = var.install_wml
+  install_wkc         = var.install_wkc
+  install_dv          = var.install_dv
+  install_spss        = var.install_spss
+  install_cde         = var.install_cde
+  install_spark       = var.install_spark
+  install_dods        = var.install_dods
+  install_ca          = var.install_ca
+  install_ds          = var.install_ds
+  install_db2oltp     = var.install_db2oltp
+  install_db2wh       = var.install_db2wh
+  install_big_sql     = var.install_big_sql
+  install_wsruntime   = var.install_wsruntime
+}
