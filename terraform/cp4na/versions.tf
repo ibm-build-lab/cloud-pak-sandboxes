2b616dd5ace93c95fb527da2fe14495870d8492a
Vyom Kumar <vyom.kumar3@ibm.com>
2021-11-11 06:42:55 -0600

    Correcting provider for cp4na

--- terraform/cp4na/versions.tf
+++ terraform/cp4na/versions.tf
@@ -1,30 +1,12 @@
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     ibm = {
-      # TF-UPGRADE-TODO
-      #
-      # No source detected for this provider. You must add a source address
-      # in the following format:
-      #
-      # source = "your-registry.example.com/organization/ibm"
-      #
-      # For more information, see the provider source documentation:
-      #
-      # https://www.terraform.io/docs/configuration/providers.html#provider-source
-      # TF-UPGRADE-TODO
-      #
-      # No source detected for this provider. You must add a source address
-      # in the following format:
-      #
-      # source = "your-registry.example.com/organization/ibm"
-      #
-      # For more information, see the provider source documentation:
-      #
-      # https://www.terraform.io/docs/configuration/providers.html#provider-source
+      source  = "ibm-cloud/ibm"
+      version = "~> 1.34"
     }
     null = {
       source = "hashicorp/null"
     }
   }
-  required_version = ">= 0.13"
 }
