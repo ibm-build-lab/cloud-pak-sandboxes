8788b578bcd58944f513cb57516b87ca8d0d3b3f
jgod360 <joel.goddot@gmail.com>
2021-12-15 11:00:07 -0500

    Update main.tf

--- terraform/cp4ba/main.tf
+++ terraform/cp4ba/main.tf
@@ -17,7 +17,8 @@ data "ibm_resource_group" "group" {
 //}
 
 module "create_cluster" {
-  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak/tree/joel_cloud_pak_terraform_issue220/modules/roks"
+//  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak/tree/joel_cloud_pak_terraform_issue220/modules/roks"
+  source = "../../../joel_cloud_pak_terraform_issue220/modules/roks"
 //  source = "../../../terraform-ibm-cloud-pak/modules/roks"
   enable               = local.enable_cluster
   on_vpc               = var.on_vpc
