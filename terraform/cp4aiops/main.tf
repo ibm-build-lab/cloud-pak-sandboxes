11b5f58194c598e514e59d1e5dff0cf124082478
jgod360 <joel.goddot@gmail.com>
2022-02-08 01:20:47 -0500

    Issue 205

--- terraform/cp4aiops/main.tf
+++ terraform/cp4aiops/main.tf
@@ -1,7 +1,7 @@
 # Provider block
 provider "ibm" {
-  region           = var.region
   ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.region
 }
 
 # Getting the OpenShift cluster configuration
@@ -17,48 +17,37 @@ resource "null_resource" "mkdir_kubeconfig_dir" {
 }
 
 module "create_cluster" {
-   source = "../../../terraform-ibm-cloud-pak/modules/roks"
+  source = "../../../terraform-ibm-cloud-pak/modules/roks" # terraform-ibm-cloud-pak/modules/roks
 //  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak/tree/terraform-0.13/modules/roks"
-  enable = local.enable_cluster
-  on_vpc = var.on_vpc
 
-  // General parameters:
-  project_name = var.project_name
-  owner        = var.owner
-  environment  = var.environment
-
-  // Openshift parameters:
+  enable               = local.enable_cluster
+  on_vpc               = var.on_vpc
+  project_name         = var.project_name
+  environment          = var.environment
+  owner                = var.owner
   resource_group       = var.resource_group
   roks_version         = var.roks_version
   flavors              = var.flavors
   workers_count        = var.workers_count
   datacenter           = var.datacenter
   force_delete_storage = true
-  vpc_zone_names       = var.vpc_zone_names
-
-  // Kubernetes Config parameters:
-  // download_config = false
-  // config_dir      = local.kubeconfig_dir
-  // config_admin    = false
-  // config_network  = false
-
-  // Debugging
-  private_vlan_number = var.private_vlan_number
-  public_vlan_number  = var.public_vlan_number
+  private_vlan_number  = var.private_vlan_number
+  public_vlan_number   = var.public_vlan_number
 }
 
-resource "null_resource" "mkdir_kubeconfig_dir" {
-  triggers = { always_run = timestamp() }
-
-  provisioner "local-exec" {
-    command = "mkdir -p ${var.cluster_config_path}"
-  }
-}
+//# getting and creation a directory for the cluster config file
+//resource "null_resource" "mkdir_kubeconfig_dir" {
+//  triggers = { always_run = timestamp() }
+//    provisioner "local-exec" {
+//    command = "mkdir -p ${var.cluster_config_path}"
+//  }
+//}
+//
 
 data "ibm_container_cluster_config" "cluster_config" {
   depends_on = [null_resource.mkdir_kubeconfig_dir]
 
-  cluster_name_id   = local.enable_cluster ? module.create_cluster.id : var.cluster_id
+  cluster_id   = local.enable_cluster ? module.create_cluster.id : var.cluster_id
   resource_group_id = module.create_cluster.resource_group.id
   config_dir        = var.cluster_config_path
   download          = true
@@ -87,8 +76,8 @@ module "install_portworx" {
   // Portworx parameters
   resource_group_name   = var.resource_group
   region                = var.region
-  cluster_id            = data.ibm_container_cluster_config.cluster_config.cluster_name_id
-  unique_id             = "px-roks-${data.ibm_container_cluster_config.cluster_config.cluster_name_id}"
+  cluster_id            = data.ibm_container_cluster_config.cluster_config.cluster_id
+  unique_id             = "px-roks-${data.ibm_container_cluster_config.cluster_config.cluster_id}"
 
   // These credentials have been hard-coded because the 'Databases for etcd' service instance is not configured to have a publicly accessible endpoint by default.
   // You may override these for additional security.
@@ -100,21 +89,21 @@ module "install_portworx" {
   etcd_secret_name      = "px-etcd-certs"
 }
 
-// TODO: With Terraform 0.13 replace the parameter 'enable' with 'count'
+//// TODO: With Terraform 0.13 replace the parameter 'enable' with 'count'
 module "install_cp4aiops" {
    source = "../../../terraform-ibm-cloud-pak/modules/cp4aiops"
-//  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak/tree/terraform-0.13/modules/cp4aiops"
-  enable = true
+  //  source = "git::https://github.com/ibm-hcbt/terraform-ibm-cloud-pak/tree/terraform-0.13/modules/cp4aiops"
+    enable = true
 
-  on_vpc              = var.on_vpc
-  portworx_is_ready   = module.install_portworx.portworx_is_ready
+    on_vpc              = var.on_vpc
+    portworx_is_ready   = module.install_portworx.portworx_is_ready
 
-  // ROKS cluster parameters:
-  cluster_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
+    // ROKS cluster parameters:
+    cluster_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
 
-  // Entitled Registry parameters:
-  entitled_registry_key        = length(var.entitled_registry_key) > 0 ? var.entitled_registry_key : file(local.entitled_registry_key_file)
-  entitled_registry_user_email = var.entitled_registry_user_email
+    // Entitled Registry parameters:
+    entitled_registry_key        = length(var.entitled_registry_key) > 0 ? var.entitled_registry_key : file(local.entitled_registry_key_file)
+    entitled_registry_user_email = var.entitled_registry_user_email
 
-  namespace = var.namespace
+    namespace = var.namespace
 }
