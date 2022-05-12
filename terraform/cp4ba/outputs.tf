3391615c06b12d4e19037a5f4bee7f39e362a014
Joel G <32661507+jgod1360@users.noreply.github.com>
2022-05-12 16:28:53 -0400

    Joel cp4ba sandbox issue 220 (#74)
    * Adding files
    
    * Deleting reductant files
    
    * tfvars template
    
    * Create .gitignore
    
    * Delete .gitignore
    
    * Create .gitignore
    
    * Issue 220
    
    * Issue 220
    
    * Issue 220
    
    * Update main.tf
    
    * Update main.tf
    
    * Issue 220
    
    * Update main.tf
    
    * Issue 223
    
    * Update main.tf
    
    * Issue 223
    
    * 220 Issue update
    
    * Issue 220 Update
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Issue 220 Update
    
    * Issue 220 Update
    
    * Issue 220 Update
    
    * Issue 220 Update
    
    * Update main.tf
    
    * Update main.tf
    
    * Issue 220 Update
    
    * Update create_db2_schemas.sh
    
    * Updating Db2 exec
    
    * Issue 74 update
    
    * Update
    
    * Update main.tf
    
    * Update
    
    * Update Issue 220
    
    * Update main.tf
    
    * Update main.tf
    
    * Delete terraform.tfvars
    
    * Update .gitignore
    
    * Update main.tf
    
    * Update exec_db2_pod.sh
    
    * Update variables.tf
    
    * Removing db2 schema files
    
    * Installer scripts update
    
    * Update main.tf
    
    * Updating installer scripts
    
    * Update create_db2_schemas.sh
    
    * Updating installer scripts
    
    * Updating tf scripts
    
    Co-authored-by: jgod1360 <--list>
    Co-authored-by: Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>

--- terraform/cp4ba/outputs.tf
+++ terraform/cp4ba/outputs.tf
@@ -1,64 +1,51 @@
+# --- ROKS output parameters
 output "cluster_id" {
-  depends_on = [
-    data.external.get_endpoints,
-  ]
-  value = var.cluster_id
+  value = local.enable_cluster ? module.create_cluster.id : var.cluster_id
 }
 
 output "cluster_name" {
-  depends_on = [
-    data.external.get_endpoints,
-  ]
-  value = join("", [var.project_name, "-", var.environment, "-cluster"])
+  value = local.enable_cluster ? module.create_cluster.name : data.ibm_container_cluster_config.cluster_config.cluster_name_id
 }
 
 output "kubeconfig" {
-  depends_on = [
-    data.external.get_endpoints,
-  ]
-  value = var.cluster_config_path
+  value = data.ibm_container_cluster_config.cluster_config.config_dir # config_file_path
 }
 
-output "db2_host_name" {
-    depends_on = [
-    data.external.get_endpoints,
-  ]
-  value = var.db2_host_name
+output "cluster_endpoint" {
+  value = local.enable_cluster == false ? var.cluster_ingress_subdomain : module.create_cluster.endpoint
 }
 
-output "db2_host_ip" {
-    depends_on = [
-    data.external.get_endpoints,
-  ]
-  value = var.db2_host_ip
+# --- Db2 outputs
+output "db2_host_address" {
+  value = var.db2_host_address ? module.install_db2.db2_host_address : local.db2_host_address
 }
 
-output "db2_port_number" {
-    depends_on = [
-    data.external.get_endpoints,
-  ]
-  value = var.db2_port_number
+output "db2_ports" {
+  value = var.enable_db2 ? module.install_db2.db2_ports : local.db2_ports
 }
 
+output "db2_pod_name" {
+  description = "This is the pod running Db2 for executing Db2 commands."
+  value = var.enable_db2 ? module.install_db2.db2_pod_name : local.db2_pod_name
+}
+
+# --- CP4BA Outputs
 output "cp4ba_endpoint" {
-  depends_on = [
-    data.external.get_endpoints,
-  ]
-  value = length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.endpoint : ""
+  description = "Access your Cloud Pak for Business Automation deployment at this URL."
+  value       = module.install_cp4ba.cp4ba_endpoint
 }
+
 
 output "cp4ba_user" {
-  depends_on = [
-    data.external.get_endpoints,
-  ]
-  value = length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.username : ""
+  value = module.install_cp4ba.cp4ba_admin_username
 }
 
 output "cp4ba_password" {
-  depends_on = [
-    data.external.get_endpoints,
-  ]
-  value = length(data.external.get_endpoints) > 0 ? data.external.get_endpoints.0.result.password : ""
+  description = "Password for your Cloud Pak for Business Automation deployment."
+  value = module.install_cp4ba.cp4ba_admin_password
 }
+
+
+
 
 
--- terraform/cp4ba/variables.tf
+++ terraform/cp4ba/variables.tf
@@ -1,3 +1,4 @@
+# --- IBM CLOUD SETTINGS ---
 variable "ibmcloud_api_key" {
   description = "IBM Cloud API key (https://cloud.ibm.com/docs/account?topic=account-userapikey#create_user_key)"
 }
@@ -12,9 +13,11 @@ variable "resource_group" {
   description = "Resource group name where the cluster will be hosted."
 }
 
-variable "project_name" {
+
+# --- ROKS SETTINGS ---
+variable "roks_project" {
   default     = "cloud-pack"
-  description = "Ignored if `cluster_id` is specified. The project_name is combined with `environment` to name the cluster. The cluster name will be '{project_name}-{environment}-cluster' and all the resources will be tagged with 'project:{project_name}'"
+  description = "Ignored if `cluster_id` is specified. The roks_name is combined with `environment` to name the cluster. The cluster name will be '{roks_name}-{environment}-cluster' and all the resources will be tagged with 'project:{roks_name}'"
 }
 
 variable "platform_version" {
@@ -65,16 +68,127 @@ variable "owner" {
   description = "Ignored if `cluster_id` is specified. Use your user name or team name. The owner is used to label the cluster and other resources with the tag 'owner:{owner}'"
 }
 
-variable "on_vpc" {
-  default     = false
-  description = "Select 'true' to install on a VPC cluster and it's using VPC Gen2. Note: CP4BA does not currently support VPC cluster."
-}
-
 variable "cluster_config_path" {
   default     = "./.kube/config"
   description = "directory to store the kubeconfig file. If running on Schematics, use `/tmp/.schematics/.kube/config`"
 }
 
+variable "cluster_ingress_subdomain" {
+  default     = ""
+  description = "The Ingress of your cluster. Ignore if there is not an existing cluster. Otherwise, for help, run the command `ibmcloud ks cluster get -c <cluster_name_or_id>` to get the Ingress Subdomain value"
+}
+
+# --- LDAP SETTINGS ---
+variable "ldap_admin_name" {
+  default = "cn=root"
+  description = "The LDAP root administrator account to access the directory. To learn more: https://www.ibm.com/docs/en/sva/7.0.0?topic=tuning-ldap-root-administrator-account-cnroot"
+}
+
+variable "ldap_admin_password" {
+  description = "LDAP Admin password"
+}
+
+
+variable "ldap_host_ip" {
+  description = "The IP address of your LDAP."
+}
+
+locals {
+  enable_cluster = var.cluster_id == null || var.cluster_id == ""
+}
+
+# --------- DB2 SETTINGS ----------
+variable "enable_db2" {
+  default     = true
+  description = "If set to true, it will install DB2 on the given cluster"
+}
+
+variable "enable_db2_schemas" {
+  default     = true
+  description = "If set to true, it will install DB2 on the given cluster"
+}
+
+ variable "db2_project_name" {
+   default     = "ibm-db2"
+   description = "The namespace/project for Db2"
+ }
+
+variable "db2_admin_username" {
+  default     = "cpadmin"
+  description = "Admin user name defined in LDAP"
+}
+
+variable "db2_user" {
+  default     = "db2inst1"
+  description = "User name defined in LDAP"
+}
+
+variable "db2_admin_user_password" {
+  description = "Db2 admin user password defined in LDAP"
+}
+
+variable "db2_name" {
+  description = "A name you would like to attribute to your Database. i.e: sample-db2"
+  default     = "sample-db2"
+}
+
+variable "db2_standard_license_key" {
+  default     = ""
+  description = "The standard license key for the Db2 database product. Note: the license key is required only for Advanced DB2 installation. Click here to download it: (https://w3-03.ibm.com/software/xl/download/ticket.wss) or [IBM Passport Advantage](https://www.ibm.com/software/passportadvantage/)"
+}
+
+variable "db2_operator_version" {
+  default     = "db2u-operator.v1.1.11"
+  description = "Operator version"
+}
+
+variable "db2_operator_channel" {
+  default     = "v1.1"
+  description = "The Operator Channel performs rollout update when new release is available."
+}
+
+variable "db2_instance_version" {
+  default     = "11.5.7.0-cn3"
+  description = "DB2 version to be installed"
+}
+
+variable "db2_cpu" {
+  default     = "4"
+  description = "CPU setting for the pod requests and limits"
+}
+
+variable "db2_memory" {
+  default     = "16Gi"
+  description = "Memory setting for the pod requests and limits"
+}
+
+variable "db2_storage_size" {
+  default     = "150Gi"
+  description = "Storage size for the db2 databases"
+}
+
+variable "db2_storage_class" {
+  default     = "ibmc-file-gold-gid"
+  description = "Name for the Storage Class"
+}
+
+variable "db2_host_address" {
+  description = "Ignore if Db2 is enabled (enable_db2 is true). Otherwise, enter existing Db2 host address. i.e: db2_host_address =  @@@@@@@@@@@@-clust-c0b572361ba41c9eef42d4d51297b04b-0000.us-south.containers.appdomain.cloud"
+  value       = ""
+}
+
+variable "db2_ports" {
+  description = "Ignore if Db2 is enabled (enable_db2 is true). Otherwise, enter existing Db2 ports. i.e: db2_ports = [01234, 56789]"
+  value       = ""
+}
+
+locals {
+  db2_pod_name     = "c-db2ucluster-db2u-0"
+  db2_host_address = var.enable_db2 == false ? var.db2_host_address : ""
+  db2_ports        = var.enable_db2 == false ? var.db2_ports : ""
+}
+
+# --------- CP4BA SETTINGS ----------
 variable "entitled_registry_user_email" {
   type        = string
   description = "Email address of the user owner of the Entitled Registry Key"
@@ -86,64 +200,13 @@ variable "entitled_registry_key" {
 }
 
 variable "cp4ba_project_name" {
-  type        = string
   default     = "cp4ba"
-  description = "namespace/project for cp4ba"
+  description = "Namespace or project for cp4ba"
 }
 
-# --- LDAP SETTINGS ---
-# Password for LDAP Admin User (ldapAdminName name see below), for example passw0rd - use the password that you specified when setting up LDAP
-variable "ldap_admin_password" {
-  description = "LDAP Admin password"
-}
-
-# LDAP instance access information - hostname or IP
-variable "ldap_server" {
-  description = "LDAP server "
-}
-
-variable "ldap_admin_name" {
-  default = "cn=root"
-  description = "The LDAP root administrator account to access the directory. To learn more: https://www.ibm.com/docs/en/sva/7.0.0?topic=tuning-ldap-root-administrator-account-cnroot"
-}
 
 locals {
-  enable_cluster = var.cluster_id == null || var.cluster_id == ""
-}
-
-# --------- DB2 SETTINGS ----------
-variable "enable_db2" {
-  default = false
-  description = "Set to 'true' if you want to install Db2 for your Cloud Pak for Business Automation. If leave 'false', it will not install Db2."
-}
-
-variable "db2_project_name" {
-   default = "ibm-db2"
-   description = "The namespace/project for Db2"
- }
-variable "db2_admin_password" {
-  description = "Db2 admin user password defined in LDAP"
-}
-
-variable "db2_admin_username" {
-  default = "db2inst1"
-  description = "Db2 admin username defined in LDAP"
-}
-
-variable "db2_host_name" {
-  description = "Host name of Db2 instance"
-}
-
-variable "db2_host_ip" {
-  description = "IP address for the Db2"
-}
-
-variable "db2_port_number" {
-  description = "Port for Db2 instance"
-}
-
-variable "db2_standard_license_key" {
-  description = "The standard license key for the Db2 database product"
+  enable_cp4ba = true
 }
 
 
